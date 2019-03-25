1268a7456c874592b681141d02d8380e3df7f639
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-03-25 13:30:17 -0500

    added framework for project

---
+++ app.js
@@ -0,0 +1,15 @@
+// Product constuctor
+
+// allProducts array
+
+// displayRandomImg function
+
+// call displayRandomImg
+
+// totalGlobalClicks var
+
+// display result function
+
+// handleClicks function that takes in event as argument
+
+// add event listener to products that calls handleClicks
